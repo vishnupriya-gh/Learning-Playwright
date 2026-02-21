c1d4aeb615d38ccaaf86b01434c6e730e25c449f
vishn <vishn@example.com>
2026-02-21 08:32:21 +0530

    Commit: save changes

---
+++ Chapter1_basics/Chapter3_Operators/01_Arithematic_Operator.js
@@ -0,0 +1,18 @@
+
+let a = 10, b = 3;
+Addition = a + b   // 13 (Addition)
+Subtraction = a - b   // 7  (Subtraction)
+Multiplication = a * b   // 30 (Multiplication)
+Division = a / b   // 3.33 (Division)
+Modulus = a % b   // 1  (Modulus - remainder)
+Exponentiation = a ** b  // 1000 (Exponentiation)
+Increment = a++     // 11 (Increment)
+Decrement = b--     // 2  (Decrement)
+console.log("Addition:",Addition);
+console.log("Subtraction:",Subtraction);
+console.log("Multiplication:",Multiplication);
+console.log("Division:",Division);  
+console.log("Modulus:",Modulus);
+console.log("Exponentiation:",Exponentiation);
+console.log("Increment:",Increment);
+console.log("Decrement:",Decrement);
---
+++ Chapter1_basics/Chapter3_Operators/03_Comparision_Operator
@@ -0,0 +1,8 @@
+console.log(5 == "5");    // true  (loose equality - compares value only)
+console.log(5 === "5");   // false (strict equality - compares value + type)
+console.log(5 != "5");    // false
+console.log(5 !== "5");   // true
+console.log(5 > 3);       // true
+console.log(5 < 3);       // false
+console.log(5 >= 5);      // true
+console.log(5 <= 4);      // false

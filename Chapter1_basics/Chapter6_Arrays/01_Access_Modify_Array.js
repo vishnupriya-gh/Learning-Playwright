09c4050534e15c4908f1650bd4a402a260065a25
vishn <vishn@example.com>
2026-03-15 22:20:54 +0530

    Your commit message

--- Chapter1_basics/Chapter6_Arrays/01_Access_Modify_Array.js
+++ Chapter1_basics/Chapter6_Arrays/01_Access_Modify_Array.js
@@ -1,2 +1,3 @@
-let strings = [Pass, "Fail","Absent"];
-console.log("")
+//**let strings = [Pass, "Fail","Absent"];
+//console.log("")
+for (let i = 0; i < 5; i++) { if (i === 3) break; } console.log(i);
